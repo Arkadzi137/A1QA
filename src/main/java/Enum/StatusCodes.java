1dd3d2641931d1ab66c30bd1932bf80f3677c8e7
Arkadzi137 <a.chervyak@qa-academy.by>
2022-10-19 19:00:03 +0300

    Add Models for all posts and users Add APIUtils

---
+++ src/main/java/Enum/StatusCodes.java
@@ -0,0 +1,18 @@
+package Enum;
+
+public enum StatusCodes {
+
+    OK(200),
+    EMPTY(404),
+    POST(201);
+
+    private int status;
+
+    StatusCodes(int status) {
+        this.status = status;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+}
